95a74b7518403cef6b8f7e47faab1c454193b483
Omair Ahmed <omair.ahmed@gmail.com>
2024-03-28 14:55:36 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,3 +11,6 @@ source ~/.zshrc
 
 # Download Auto-Completion
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Download Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting

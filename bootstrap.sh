65afcdd815e2185ba3492e9fe7ef7d3b6411d75c
ofa1 <omair.ahmed@gmail.com>
2025-04-12 22:42:37 -0400

    Add additional Zsh plugins and update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,20 @@ ln -sf ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh
 
 source ~/.zshrc
 
+# Install plugins
+
+# zsh-fast-syntax-highlighting plugin
+
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+# zsh-autocomplete plugin
+
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+# zsh-fzf plugin 
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+
+
 # Download Auto-Completion
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 

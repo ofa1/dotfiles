efe4ad36e92556c051795d142d7deddc55ea2e7c
Omair Ahmed <omair.ahmed@gmail.com>
2025-06-09 13:46:43 -0400

    Enhance bootstrap script and Zsh configuration: add checks for existing installations, streamline plugin setup, and set ZSH variables in zshrc.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,21 +3,27 @@
 # if MacOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # Install Homebrew if not installed
-  if ! command -v brew &> /dev/null; then
+  if ! command -v brew &>/dev/null; then
     echo "Homebrew not found. Installing..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
   # Install zsh if not installed
-  if ! command -v zsh &> /dev/null; then
+  if ! command -v zsh &>/dev/null; then
     echo "Zsh not found. Installing..."
     brew install zsh
   fi
 
   # Install git if not installed
-  if ! command -v git &> /dev/null; then
+  if ! command -v git &>/dev/null; then
     echo "Git not found. Installing..."
     brew install git
+  fi
+
+  # Install Graphite CLI if not installed
+  if ! command -v gt &>/dev/null; then
+    echo "Graphite CLI not found. Installing..."
+    brew install graphite
   fi
 
 else
@@ -25,42 +31,69 @@ else
   exit 1
 fi
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install Oh My Zsh if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+  echo "Oh My Zsh already installed, skipping..."
+fi
 
 # Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+if [ ! -d "$HOME/.fzf" ]; then
+  echo "Installing fzf..."
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --key-bindings --completion --no-update-rc
+else
+  echo "fzf already installed, skipping..."
+fi
 
-# ZSH
-ln -sfn ~/dotfiles/zsh ~/.zsh
-ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+# ZSH config symlinks
+echo "Setting up ZSH configuration..."
+DOTFILES_PATH="${0:A:h}"
+ln -sfn "$DOTFILES_PATH/zsh" "$HOME/.zsh"
+ln -sf "$DOTFILES_PATH/zsh/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_PATH/zsh/p10k.zsh" "$HOME/.p10k.zsh"
 
-source ~/.zshrc
+# Set ZSH_CUSTOM if not set
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
 # Install plugins
+echo "Installing ZSH plugins..."
 
 # zsh-fast-syntax-highlighting plugin
-
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+if [ ! -d "$ZSH_CUSTOM/plugins/fast-syntax-highlighting" ]; then
+  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZSH_CUSTOM/plugins/fast-syntax-highlighting
+fi
 
 # zsh-autocomplete plugin
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autocomplete" ]; then
+  git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+fi
 
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+# zsh-fzf-tab plugin
+if [ ! -d "$ZSH_CUSTOM/plugins/fzf-tab" ]; then
+  git clone https://github.com/Aloxaf/fzf-tab $ZSH_CUSTOM/plugins/fzf-tab
+fi
 
-# zsh-fzf plugin 
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+# zsh-autosuggestions plugin
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
 
+# zsh-syntax-highlighting plugin
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
 
-# Download Auto-Completion
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# zsh-completions plugin
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-completions" ]; then
+  git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
+fi
 
-# Download Syntax Highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Install powerlevel10k theme
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+fi
 
-# Download powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# Download fzf-tab
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+echo "Bootstrap complete! Please restart your terminal or run: source ~/.zshrc"
--- zsh/config/variables.zsh
+++ zsh/config/variables.zsh
@@ -1,12 +1,7 @@
 # Global variables
 # --------------------------------------
 
-# Oh My ZSH
-export ZSH=$HOME/.oh-my-zsh
-export ZSH_THEME="powerlevel10k/powerlevel10k"
-
 # Path
 export PATH="$HOME/.zsh/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
-
